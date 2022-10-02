88926775b945fb40aad7e0749331b18f16f1a1bf
Любовь Кравченко <kravchenkoluba@yandex.ru>
2022-10-02 13:26:43 +0300

    Изменение концепции работы, удаление связи с контроллером

--- ui.py
+++ ui.py
@@ -1,6 +1,5 @@
 
 import check 
-import controller
 import logger
 
 def print_ui(data):  # Метод выводит в консоль то, что получил 
@@ -15,20 +14,84 @@ def hello_user(): # Выводит приветствие
     print('Добро пожаловать! Вы в приложении справочник.')
 
 
-def ui_input_new_data(y:str):  # Превращает строку в список словарей, где ключ - номер элемента строки, значение - сам элемент.
-    y = y.upper()              # Возвращает список словарей
-    y = y.split(' ')
-    lst_dict = []
-    for i in range(len(y)):
-        lst_dict.append({i:y[i]})
-    return lst_dict
+def make_lst_of_dict_for_add(): # Для добавления данных 
+    t_str = 'Данные введены некорректно'
+    print('Введите данные для добавления.')
+    while True:
+        surname = input('Введите фамилию: ')
+        logger.dif_log(surname)
+        if not surname.isalpha:
+            logger.dif_log(t_str)
+            print(t_str)
+            continue
+        else:
+            while True:
+                name = input('Введите имя: ')
+                logger.dif_log(name)
+                if not name.isalpha:
+                    logger.dif_log(t_str)
+                    print(t_str)
+                    continue
+                else:
+                    while True:
+                        fathername = input('Введите отчество: ')
+                        logger.dif_log(fathername)
+                        if not fathername.isalpha:
+                            logger.dif_log(t_str)
+                            print(t_str)
+                            continue
+                        else:
+                            while True:
+                                telefon = input('Введите телефон: ')
+                                logger.dif_log(telefon)
+                                if not telefon.isdigit:
+                                    logger.dif_log(t_str)
+                                    print(t_str)
+                                    continue
+                                else:
+                                    while True:
+                                        comment = input('Введите комментарий: ')
+                                        logger.dif_log(comment)
+                                        if not comment.isalpha:
+                                            logger.dif_log(t_str)
+                                            print(t_str)
+                                            continue
+                                        else:
+                                            lst_dict = [{'surname':surname},{'name':name},{'fathername':fathername},{'telefon':telefon}, {'comment':comment}]
+                                        return lst_dict
 
 
-def make_lst_of_dict_for_add(x:str): # Превращает строку в список словарей, где ключ уже задан, значение - элементы строки по порядку. 
-    x = x.upper()                    # Возвращает список словарей
-    x = x.split(' ')
-    lst_dict = [{'surname':x[0]},{'name':x[1]},{'fathername':x[2]},{'telefon':x[3]}, {'comment':x[4]}]
-    return lst_dict
+def ui_input_new_data():  # Для поиска
+    t_str = 'Данные введены некорректно'
+    print('Введите данные для поиска. В ненужные поля ставьте пробел.')
+    while True:
+        surname = input('Введите фамилию: ')
+        logger.dif_log(surname)
+        if not check.is_number(surname):
+            logger.dif_log(t_str)
+            print(t_str)
+            continue
+        else:
+            while True:
+                name = input('Введите имя: ')
+                logger.dif_log(name)
+                if not check.is_number(name):
+                    logger.dif_log(t_str)
+                    print(t_str)
+                    continue
+                else:
+                    while True:
+                        fathername = input('Введите отчество: ')
+                        logger.dif_log(fathername)
+                        if not check.is_number(fathername):
+                            logger.dif_log(t_str)
+                            print(t_str)
+                            continue
+                        else:
+                            lst_dict = [{'surname':surname},{'name':name},{'fathername':fathername}]
+                            return lst_dict
+
+  
 
 
 def format_operation(name_operation):   # Принимает название операции, возвращает цифру - какой формат файла создастся в процессе операции
@@ -84,31 +147,19 @@ def ui_menu():  # Основное меню
             continue
         user_input = int(user_input)
         if user_input == 1:
-            user_data = input('Введите данные для поиска через пробел: ')
-            logger.dif_log(user_data)
-            # проверка введеннных данных?
-            new_user_data = ui_input_new_data(user_data)
-            rez = controller.action(user_input, new_user_data)
+            new_user_data = ui_input_new_data()
+            return user_input, new_user_data
         elif user_input == 2:
-            user_data = input('Введите данные для добавления  в формате -> Фамилия Имя Отчество Телефон Комментарий. Данные вводятся через пробел')
-            logger.dif_log(user_data)
-            # проверка введеннных данных?
-            new_user_data = make_lst_of_dict_for_add(user_data)
-            rez = controller.action(user_input, new_user_data)
+            new_user_data = make_lst_of_dict_for_add()
+            return user_input, new_user_data
         elif user_input == 3:
-            user_data = format_operation('импорт')
-            logger.dif_log(user_data)
-            # проверка введеннных данных?
-            rez = controller.action(user_input, user_data)
+            return user_input
         elif user_input == 4:
-            user_data = format_operation('экспорт')
-            logger.dif_log(user_data)
-            # проверка введеннных данных?
-            rez = controller.action(user_input, user_data)
+            return user_input
         elif user_input == 5:
             print('До свидания!')
             exit()
-        return rez
+        
 
 
 def ui_podmenu():  # Подменю
@@ -133,42 +184,49 @@ def ui_podmenu():  # Подменю
         return user_input
 
 
-def input_data_for_change(): # Метод для редактирования данных
+def input_data_for_change():    # Для редактирования данных
+    t_str = 'Данные введены некорректно'
+    print('Введите данные для редактирования.')
     while True:
-        print('Выберите что будем редактировать\n'
-        '1 - фамилию\n'
-        '2 - имя\n'
-        '3 - отчество\n'
-        '4 - телефон\n'
-        '5 - комментарий')
-        user_input = input('и введите цифру: ')
-        logger.dif_log(user_input)
-        if not check.is_number(user_input, 1, 5):
-            t_str = 'Данные введены некорректно'
+        surname = input('Введите новую фамилию: ')
+        logger.dif_log(surname)
+        if not surname.isalpha:
             logger.dif_log(t_str)
             print(t_str)
             continue
-        user_input = int(user_input)
-        if user_input == 1:
-            user_data = input('Введите новую фамилию: ')
-            # проверка
-            new_user_data = [{'surname':user_data}]
-        elif user_input == 2:
-            user_data = input('Введите новое имя: ')
-            # проверка
-            new_user_data = [{'name':user_data}]
-        elif user_input == 3:
-            user_data = input('Введите новое отчество: ')
-            # проверка
-            new_user_data = [{'fathername':user_data}]
-        elif user_input == 4:
-            user_data = input('Введите новый телефон: ')
-            # проверка
-            new_user_data = [{'telefon':user_data}]
-        elif user_input == 5:
-            user_data = input('Введите новый комментарий: ')
-            # проверка
-            new_user_data = [{'comment':user_data}]
-        return new_user_data
-
+        else:
+            while True:
+                name = input('Введите новое имя: ')
+                logger.dif_log(name)
+                if not name.isalpha:
+                    logger.dif_log(t_str)
+                    print(t_str)
+                    continue
+                else:
+                    while True:
+                        fathername = input('Введите новое отчество: ')
+                        logger.dif_log(fathername)
+                        if not fathername.isalpha:
+                            logger.dif_log(t_str)
+                            print(t_str)
+                            continue
+                        else:
+                            while True:
+                                telefon = input('Введите новый телефон: ')
+                                logger.dif_log(telefon)
+                                if not check.is_telefon:
+                                    logger.dif_log(t_str)
+                                    print(t_str)
+                                    continue
+                                else:
+                                    while True:
+                                        comment = input('Введите новый комментарий: ')
+                                        logger.dif_log(comment)
+                                        if not comment.isalpha:
+                                            logger.dif_log(t_str)
+                                            print(t_str)
+                                            continue
+                                        else:
+                                            lst_dict = [{'surname':surname},{'name':name},{'fathername':fathername},{'telefon':telefon}, {'comment':comment}]
+                                        return lst_dict
 
