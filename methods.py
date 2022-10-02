3126c3a3c9eb62e31891d52f45a75045c4ea1e9e
Rashit <gorinnose@yandex.ru>
2022-10-02 16:50:31 +0300

    Модуль методы. Реализовано: добавление, удаление, редактирование. Не сделано: поиск, сортировка по столбцу и перенумерация id

--- methods.py
+++ methods.py
@@ -42,7 +42,24 @@ def create_dict_empty():
     }
     return mydict
 
-def m_adding(name_path_file : str, record : list) -> int:
+# def renumirate_id_column(list_db : list) -> list:
+#     ''' 
+#     функция для перенумерации id по порядку в bd
+
+#     Вход:
+#        список словарей - исходная база данных
+#     Возвращает:
+#         пустой словарь для дальнейшего его заполненнения
+#     Пояснение:
+#         1. Окрытие файла на чтение .json
+#         2. Сохранение в типе данных json
+#         3. Закрытие файла .json
+#         4. Поиск строк по нужному критерию
+#         5. Формирование нового словаря из результата поиска
+#     '''
+
+
+def m_adding(name_path_file : str, record : list) -> list or int:
     '''
     функия для добавления записи(-ей) в конец БД (файла .json)
     
@@ -56,35 +73,73 @@ def m_adding(name_path_file : str, record : list) -> int:
     Значение:
         штатное выполнение - возвращает БД в виде списка словарей (для выводав консоль)
         сбой при выполнении - (-1) для отправки сообщения из контроллера в ui
+    
     Пояснение:
-        1. Поверка передали 1 запись в БД или несколько записей в БД (сколько словарей в списке)
+        1. Передали 1 запись в БД или несколько записей в БД
         2. Преобразование в .json
-        3. Окрытие файла для добавления и запись в конец
+        3. Окрытие файла для добавления и перезапись
+    Примечание!
+        В файле читаемом должно быть записано в виде списка с []!
+                файл.json:
+                [   <-это обязательно!
+                    {
+                        "id": 1,
+                        "surname": "Иванов",
+                        "name": "Иван",
+                        "fathername": "Иванович",
+                        "telefon": 89270010101,
+                        "comment": "телефон Иванов"
+                    }
+                ]  <-это обязательно!
     '''
     try:
-        import json                                 # импортируем библиотеку
-        with open(name_path_file, "a+", encoding='utf-8') as my_file:  # Записываем в файл
-            for i in range(0, len(record)):
-                string_json = json.dumps(record[i]) # сериализуем его в JSON-структуру, как строку
-                # print(string_json)
-                my_file.write(f'{string_json}\r')   # Записываем в файл с возвратом корретки
-        return 1
+        import json                                                     # импортируем библиотеку
+        # Действие 1 - считать исходную БД в переменную
+        with open(name_path_file, "r", encoding="UTF-8") as my_file:    # читаем из файла
+            string_json = my_file.read()
+        # Eсли файл пустой
+        if string_json == "":                                           
+            for i in range(0, len(record)):                             # задаём id c 1 и далее
+                (record[i])['id'] = i + 1 
+            string_json = json.dumps(record, indent=4, ensure_ascii=False) # десериализация в строку json
+            with open(name_path_file, "w", encoding="UTF-8") as my_file:
+                my_file.write(string_json)
+            return record
+        # Иначе если файлне пустой, а уже с имеющимся списком
+        list_readed_dicts = json.loads(string_json)                      # проводим десериализацию JSON-объекта
+        # Действие 2 - обновление индексов при добавлении
+        # Действие 2.1 - поиска последнего индекса в исходной БД
+        count_records_in_base_db = len(list_readed_dicts)                # количество записей/строк
+        last_dict_in_db = list_readed_dicts[count_records_in_base_db-1]  # ID из последней записи/строки
+        id_of_last_dict = last_dict_in_db['id']
+        # Действие 2.2 - перезапись id в добавляемой записи(-ей)
+        for i in range(0, len(record)):              
+            (record[i])['id'] = id_of_last_dict + i + 1                 # приращение id в добавляемых записях  
+        # Действие 3 - Добавить данные в конец БД полностью весь новый список
+        list_readed_dicts.extend(record)
+        # Действие 4 - Перезаписать полностью обновлённую БД c имеющимся обновлением в конце
+        string_json = json.dumps(list_readed_dicts, indent=4, ensure_ascii=False) # десериализация в строку json
+        with open(name_path_file, "w", encoding="UTF-8") as my_file:
+            my_file.write(string_json)
+        return list_readed_dicts
     except:
         return -1
 
-def m_delete(name_path_file : str, id_value : int) -> int:
+def m_delete(name_path_file : str, id_value : int) -> list or int:
     '''
     функия удаления записи указанной позиции в БД (файла .json)
+    
     Аргументы:
         name_path_file  - тип данных "str | строка"
                         в данной реализации имя файла БД с расширением .json
                         без указания пути к файлу (корневой каталог с файлом main.py)
-        id_value - тип данных список из "int"
+        id_value - тип 1 число в "int"
                     номер или id-записи для удаления из БД
 
     Значение:
-        1 - штатное выполнение
-        -1 - сбой при выполнении, для отправки сообщения из контроллера в ui
+        штатное выполнение - возвращает БД в виде списка словарей (для выводав консоль)
+        сбой при выполнении - (-1) для отправки сообщения из контроллера в ui
+    
     Пояснение:
         1. Окрытие файла на чтение .json
         2. Сохранение в типе данных json
@@ -94,43 +149,92 @@ def m_delete(name_path_file : str, id_value : int) -> int:
         6. Перезапись файла в типе данных json
         3. Закрытие файла
     '''
-    # try:
-    #     import json                                 # импортируем библиотеку
-    #     with open(name_path_file, "r", encoding='utf-8') as my_file:  # Записываем в файл
-    #         string_json = my_file.readlines()
-    # #     string_json 
+    try:
+        import json                                                     # импортируем библиотеку
+        # Действие 1 - считать исходную БД в переменную
+        with open(name_path_file, "r", encoding="UTF-8") as my_file:    # читаем из файла
+            string_json = my_file.read()
+        list_readed_dicts = json.loads(string_json)                      # проводим десериализацию JSON-объекта
+        # Действие 2 - Удаление по id из списка словарей (БД из файла)
+        # Действие 2.1 - Поиск индекса списка по номеру id
+        index_of_record_with_needed_id = -1                             # если такого id нет, то удалить не можем
+        for i in range(0, len(list_readed_dicts)):                     
+            if (list_readed_dicts[i])['id'] == id_value:                # сохранить индекс словаря в списке с нужным id
+                index_of_record_with_needed_id = i
+                break  # так как id уникальный первичный ключ, одинаковых значений не может быть
+        # Действие 2.2 - Непосредственное удаление по найденному индексу (не id)
+        if index_of_record_with_needed_id == -1:
+            return -1    #если не нашли id в справочнике для удаления, то выходим с отрицательным результатом выполнения
+        del(list_readed_dicts[index_of_record_with_needed_id])          # удаление элемента списка по индексу
+        # Действие 3 - Перезаписать полностью обновлённую БД с отсутсвием удалённой записи
+        string_json = json.dumps(list_readed_dicts, indent=4, ensure_ascii=False) # десериализация в строку json
+        with open(name_path_file, "w", encoding="UTF-8") as my_file:
+            my_file.write(string_json)
+        return list_readed_dicts
+    except:
+        return -1
 
-    #     for i in range(0, len(record)):
-    #         string_json = json.dumps(record[i]) # сериализуем его в JSON-структуру, как строку
-    #         # print(string_json)
-    #         my_file.write(f'{string_json}\r')  # Записываем в файл с возвратом корретки
-    #     return 1
-    # except:
-    #     return -1
-
-def m_edit(name_path_file : str, id_value : int) -> int:
+def m_edit(name_path_file : str, record : list) ->  list or int:
     '''
-    функия редактирования записи указанной позиции в БД (файла .json)
+    функция редактирования записи по указанному id в БД (файла .json) 
+        id и значения для изменения хранятся в словаре/списке
+
     Аргументы:
         name_path_file  - тип данных "str | строка"
                         в данной реализации имя файла БД с расширением .json
                         без указания пути к файлу (корневой каталог с файлом main.py)
-        id_value - тип данных список из "int"
-                    fномер или id-записи для редактирования из БД
-
+        record_with_changes - список словарей для редактирования, все заполненные значения ключей на перезапись в основную БД
+    
     Значение:
-        1 - штатное выполнение
-        -1 - сбой при выполнении, для отправки сообщения из контроллера в ui
+        штатное выполнение - возвращает БД в виде списка словарей (для выводав консоль)
+        сбой при выполнении - (-1) для отправки сообщения из контроллера в ui
+
     Пояснение:
-        1. Окрытие файла на чтение .json
-        2. Сохранение в типе данных json
-        3. Закрытие файла .json
-        4. Замена строки с нужным номером id
-        5. Открытие файла перезапись .json
-        6. Перезапись файла в типе данных json
-        3. Закрытие файла
+        1. Окрытие файла на чтение .json + Сохранение в типе данных json + Закрытие файла .json
+        2. Замена строки (словаря в списке) с нужными данными по номеру id, выбранному пользователем и переданным в словаре (списке)
+        3. Открытие файла перезапись .json + Перезапись файла в типе данных json +Закрытие файла
     '''
-    pass
+    try:
+        import json                                                     # импортируем библиотеку
+        # Действие 1 - считать исходную БД в переменную
+        with open(name_path_file, "r", encoding="UTF-8") as my_file:    # читаем из файла
+            string_json = my_file.read()
+        list_readed_dicts = json.loads(string_json)                      # проводим десериализацию JSON-объекта
+        # Действие 2 - Редактирование по id из списка словарей (БД из файла)
+        # Действие 2.1 - Поиск индекса списка по номеру id
+        index_of_record_with_needed_id = -1                             # если такого id нет, то редактировать не можем
+        # Если случайно предали искомое как словарь - перезапись его в списк из словарей
+        if (type(record) is dict):
+            record = [record]
+        id_record_to_modify = (record[0])['id']                         # вытащено id из словаря/списка, выбранного пользователем на изменение
+        for i in range(0, len(list_readed_dicts)):                      
+            if (list_readed_dicts[i])['id'] == id_record_to_modify:                
+                index_of_record_with_needed_id = i
+                break  # так как id уникальный первичный ключ, одинаковых значений не может быть
+        # Действие 2.2 - Непосредственное редактирование по найденномув п. 2.1 индексу (уже не по не id)
+        if index_of_record_with_needed_id == -1:
+            return -1    #если не нашли id в справочнике для удаления, то выходим с отрицательным результатом выполнения
+        i = index_of_record_with_needed_id
+        # !!!!Вот это очень нехорошая часть!!!!!
+        # так как при изменении сруктуры БД (+- столбец|ключ) всё полетит
+        # for key, value in users.items(): print(f"Phone: {key}  User: {value} ")  не даст сразу 2 перебрать
+        if (record[0])['surname'] != "":
+            (list_readed_dicts[i])['surname'] = (record[0])['surname']
+        if (record[0])['name'] != "":
+            (list_readed_dicts[i])['name'] = (record[0])['name']
+        if (record[0])['fathername'] != "":
+            (list_readed_dicts[i])['fathername'] = (record[0])['fathername']
+        if (record[0])['telefon'] != 0:
+            (list_readed_dicts[i])['telefon'] = (record[0])['telefon']
+        if (record[0])['comment'] != "":
+            (list_readed_dicts[i])['comment'] = (record[0])['comment']
+         # Действие 3 - Перезаписать полностью обновлённую БД с отсутсвием удалённой записи
+        string_json = json.dumps(list_readed_dicts, indent=4, ensure_ascii=False) # десериализация в строку json
+        with open(name_path_file, "w", encoding="UTF-8") as my_file:
+            my_file.write(string_json)
+        return list_readed_dicts
+    except:
+        return -1
 
 def m_search(name_path_file : str, input_str : str) -> list:
     '''
@@ -177,3 +281,132 @@ def m_search(name_path_file : str, input_str : str) -> list:
 # f = make_incrementor(42)
 # f(0)   ->   42
 # f(1)   ->   43
+
+
+
+#****************************************************************************** 
+#********************************* НАЧАЛО ТЕСТОВ ****************************** 
+#****************************************************************************** 
+def give_me_any_list_of_dict():
+
+    test_dict = [
+        {
+            "id": 1,
+            "surname": "Иванов",
+            "name": "Иван",
+            "fathername": "Иванович",
+            "telefon": 89270010101,
+            "comment": "телефон Иванов"
+        },
+        {
+            "id": 2,
+            "surname": "Петров",
+            "name": "Петр",
+            "fathername": "Петрович",
+            "telefon": 89270020202,
+            "comment": "телефон Петрова"
+        },
+        {
+            "id": 3,
+            "surname": "Степанов",
+            "name": "Степан",
+            "fathername": "Степанович",
+            "telefon": 89270030303,
+            "comment": "телефон Степанова"
+        },
+        {
+            "id": 4,
+            "surname": "Сидоров",
+            "name": "Иван",
+            "fathername": "Петрович",
+            "telefon": 89270040404,
+            "comment": "телефон Сидорова"
+        }
+    ]
+    return test_dict
+
+print("\n Выполнение тестов")
+## Получить список по умолчанию из 4-х записей/словарей
+dict_test = give_me_any_list_of_dict()
+# print("\nPRIMARY (not file) dict_test = ", dict_test)
+name_file = "test_alex.json"
+
+#****************************************************************************** 
+#************ ТЕСТ № 1 - ДОБАВЛЕНИЕ В КОНЕЦ ТЕЛЕФОННОГО СПРАВОЧНИКА *********** 
+#****************************************************************************** 
+# #для добавления в конец
+# dict_to_add = [
+#         {
+#             "id": 1,
+#             "surname": "Иванов",
+#             "name": "Иван",
+#             "fathername": "Иванович",
+#             "telefon": 89270010101,
+#             "comment": "телефон Иванов"
+#         }
+# ]
+# print("\ndict_to_add = ", dict_to_add)
+# #Добавление новой записи в конец
+# dict_test_updated = m_adding(name_file, dict_test)
+# print("\ndict_test_updated = ", dict_test_updated)
+
+#****************************************************************************** 
+#******** ТЕСТ № 2 - УДАЛЕНИЕ ЗАПИСИ ПО ID В ТЕЛЕФОННОМ СПРАВОЧНИКЕ *********** 
+#****************************************************************************** 
+# # Удаление записи по номеру id
+# dict_test_minus_one_by_id = m_delete(name_file, 5)
+# print("\n dict_test_minus_one_by_id = ", dict_test_minus_one_by_id)
+
+#****************************************************************************** 
+#******** ТЕСТ № 3 - ИЗМЕНЕНИЕ ЗАПИСИ ПО ID В ТЕЛЕФОННОМ СПРАВОЧНИКЕ ********** 
+#****************************************************************************** 
+# Список из словарей для изменения
+list_dict_with_changes = [
+        {
+            "id": 1,
+            "surname": "Гвидонов",
+            "name": "Гвидон",
+            "fathername": "Гвидонович",
+            "telefon": 0,
+            "comment": ""
+        }
+]
+# Словарь для изменения
+dict_with_changes = \
+        {
+            "id": 1,
+            "surname": "Словарёв",
+            "name": "Словарь",
+            "fathername": "Словаревич",
+            "telefon": 0,
+            "comment": "Демо со словарём"
+        }
+
+# ! сначала нужн очистить файл "test_alex.json"
+m_adding(name_file, give_me_any_list_of_dict())
+
+# демонстрация успеха с списком словарей в аргументах
+dict_test_midifited_by_id = m_edit(name_file, list_dict_with_changes)
+print("\n Успех со списком - dict_test_midifited_by_id = ", dict_test_midifited_by_id)
+
+# демонстрация провала с списком словарей в аргументах (т.к. записи нет с id = 100)
+list_dict_with_changes_second = list_dict_with_changes
+(list_dict_with_changes_second[0])['id'] = 100
+dict_test_midifited_by_id = m_edit(name_file, list_dict_with_changes_second)
+print("\nПровал - dict_test_midifited_by_id = ", dict_test_midifited_by_id)
+
+# демонстрация успеха с словарём в аргументах
+dict_test_midifited_by_id = m_edit(name_file, dict_with_changes)
+print("\n Успех со словарём - dict_test_midifited_by_id = ", dict_test_midifited_by_id)
+
+# демонстрация успеха с списком словарей в аргументах
+(list_dict_with_changes[0])['id'] = 1
+dict_test_midifited_by_id = m_edit(name_file, list_dict_with_changes)
+print("\n Успех со списком - dict_test_midifited_by_id = ", dict_test_midifited_by_id)
+
+#****************************************************************************** 
+#************ ТЕСТ № 4 - ПОИСК ЗАПИСИ  В ТЕЛЕФОННОМ СПРАВОЧНИКЕ *************** 
+#****************************************************************************** 
+
+
+print("\nЗавершение тестов\n ")
