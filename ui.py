af11c4c58f2ea2e2c4fbcc890039b8bbfc635bd2
Любовь Кравченко <kravchenkoluba@yandex.ru>
2022-10-03 12:11:51 +0300

    Актуальная версия ui

--- ui.py
+++ ui.py
@@ -120,22 +120,6 @@ def ui_menu():  # Основное меню
             print('До свидания!')
             exit()
         return int(user_input)
-        return int(user_input)
-
-        # if user_input == 1:
-        #     new_user_data = ui_input_new_data()
-        #     return user_input, new_user_data
-        # elif user_input == 2:
-        #     new_user_data = make_lst_of_dict_for_add()
-        #     return user_input, new_user_data
-        # elif user_input == 3:
-        #     return user_input
-        # elif user_input == 4:
-        #     return user_input
-        # elif user_input == 5:
-        #     print('До свидания!')
-        #     exit()
-        
 
 
 def ui_podmenu():  # Подменю
